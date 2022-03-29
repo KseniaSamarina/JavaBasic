2a0a6d1e9855ee52a8b4d2d608d632b126137c2e
KseniaSamarina <kseniya.samarina@yandex.ru>
2022-03-29 20:30:16 +0300

    Выполнила второе практическое задание - Типы данных

---
+++ src/JavaBasic/DataTypes.java
@@ -0,0 +1,10 @@
+package JavaBasic;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        double number = 19.031997;
+        byte num = 127;
+        System.out.println(number);
+        System.out.println(num);
+    }
+}
