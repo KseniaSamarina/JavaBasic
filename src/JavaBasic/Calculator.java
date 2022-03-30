a2646cba83f3dee9467718630d501186f84e15e6
KseniaSamarina <kseniya.samarina@yandex.ru>
2022-03-30 14:25:31 +0300

    Выполнила пятое практическое задание - Калькулятор

---
+++ src/JavaBasic/Calculator.java
@@ -0,0 +1,41 @@
+package JavaBasic;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.println("Пожалуйста, введите первое число");
+                int x = console.nextInt();
+                System.out.println("Пожалуйста, введите второе число");
+                int y = console.nextInt();
+                System.out.println("Пожалуйста, введите символ арифметической операции");
+                char z = console.next().charAt(0);
+
+                if (z == '+') {
+                    System.out.println("Сумма = " + (x + y));
+                } else if (z == '-') {
+                    System.out.println("Разность = " + (x - y));
+                } else if (z == '*') {
+                    System.out.println("Произведение = " + (x * y));
+                } else if (z == '/') {
+                    System.out.println("Частное от деления = " + (x / y));
+                } else {
+                    System.out.println("Введён несуществующий символ");
+                }
+            } catch (Exception e) {
+                System.err.println("Введена неизвестная команда");
+            }
+            System.out.println("Вы хотите продолжить(y/n)?");
+            char m = console.next().charAt(1);
+            if (console.next().equals("n")) {
+                break;
+            }
+        }
+    }
+}
+
+
